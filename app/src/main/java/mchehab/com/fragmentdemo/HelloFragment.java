8079877dab409928c76174f67a4ac420b4d0775c
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-22 17:26:10 +0200

    Created UI for fragment

--- app/src/main/java/mchehab/com/fragmentdemo/HelloFragment.java
+++ app/src/main/java/mchehab/com/fragmentdemo/HelloFragment.java
@@ -2,11 +2,17 @@ package mchehab.com.fragmentdemo;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HelloFragment extends Fragment {
+
+    private EditText editText;
+    private Button button;
 
     public HelloFragment() {
         // Required empty public constructor
@@ -23,6 +29,17 @@ public class HelloFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_hello, container, false);
+        View view = inflater.inflate(R.layout.fragment_hello, container, false);
+        editText = view.findViewById(R.id.editText);
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(e-> {
+            new AlertDialog.Builder(getActivity())
+                    .setTitle("Click Detected")
+                    .setMessage(editText.getText().toString())
+                    .setPositiveButton("Dismiss", null)
+                    .create()
+                    .show();
+        });
+        return view;
     }
 }
