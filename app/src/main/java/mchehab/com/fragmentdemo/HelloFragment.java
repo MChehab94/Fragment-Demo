a6348607a66ff5bc36c1177b2cae581cefa1f07f
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-23 16:16:29 +0200

    Changed root layout to Linear and added three EditTexts

--- app/src/main/java/mchehab/com/fragmentdemo/HelloFragment.java
+++ app/src/main/java/mchehab/com/fragmentdemo/HelloFragment.java
@@ -2,17 +2,16 @@ package mchehab.com.fragmentdemo;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class HelloFragment extends Fragment {
 
-    private EditText editText;
-    private Button button;
+    private EditText editTextFirstName;
+    private EditText editTextLastName;
+    private EditText editTextDetails;
 
     public HelloFragment() {
         // Required empty public constructor
@@ -30,16 +29,9 @@ public class HelloFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_hello, container, false);
-        editText = view.findViewById(R.id.editText);
-        button = view.findViewById(R.id.button);
-        button.setOnClickListener(e-> {
-            new AlertDialog.Builder(getActivity())
-                    .setTitle("Click Detected")
-                    .setMessage(editText.getText().toString())
-                    .setPositiveButton("Dismiss", null)
-                    .create()
-                    .show();
-        });
+        editTextFirstName = view.findViewById(R.id.editTextFirstName);
+        editTextLastName = view.findViewById(R.id.editTextLastName);
+        editTextDetails = view.findViewById(R.id.editTextDetails);
         return view;
     }
 }
